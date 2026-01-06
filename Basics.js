dbc00945fbdd7cfdcadc41ad2bf50d63b6fb1a07
Vidhyagnath <vidhyaig01@gmail.com>
2026-01-06 16:16:47 +0530

    Add files via upload
    Added basic GEE JavaScript practice

---
+++ Basics.js
@@ -0,0 +1,27 @@
+///variable
+var city='Bengaluru';
+var state='Karnataka';
+print(state,city)
+
+var population='840000';
+print(population);
+
+///list
+var largecities=['Tokyo', 'Delhi', 'Shanghai', 'Mexico City', 'Sao Paulo']
+print(largecities)
+
+///dictionary
+var CityData= {
+  'City':city,
+  'population':870000,
+  'elevation':930,
+  
+};
+print(CityData)
+
+///function
+
+var greet=function(name){
+  return "Hello"+ name
+};
+print(greet('World'))
